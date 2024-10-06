80d06374f7be18a25bf26cfbed432b663bd8a1da
jhines37 <jaredjhines@gmail.com>
2024-10-06 18:25:30 -0400

    Update GTIMC Script.py

--- GTIMC Script.py
+++ GTIMC Script.py
@@ -1,19 +1,21 @@
-## Script to fetch Data for DCF Model API Key: "H1PL45YBNI1IZDJU"
+## Script to fetch Data for DCF Model 
+## API Documentation "https://www.alphavantage.co/documentation/" more APIs and get an API Key from the link
 import requests
 import json
 import pandas as pd
 
+apikey = input("What is your API key? ")
 ticker = input("What ticker? ")
 
 # Fetch Income Statement Data
-incomeStatementURL = 'https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=' + ticker + '&apikey=H1PL45YBNI1IZDJU'
+incomeStatementURL = 'https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=' + ticker + '&apikey=' + apikey
 requestIncomeStatementData = requests.get(incomeStatementURL)
 incomeStatmentData = requestIncomeStatementData.json()
 df = pd.DataFrame(incomeStatmentData['annualReports'])
 df.to_csv('IncomeStatmentData.csv', index=False)
 
 # Fetch Balance Sheet Data
-balanceSheetURL = 'https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol=' + ticker + '&apikey=H1PL45YBNI1IZDJU'
+balanceSheetURL = 'https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol=' + ticker + '&apikey=' + apikey
 requestBalanceSheetData = requests.get(balanceSheetURL)
 balanceSheetData = requestBalanceSheetData.json()
 print(balanceSheetData)
@@ -21,7 +23,7 @@ df = pd.DataFrame(balanceSheetData['annualReports'])
 df.to_csv('BalanceSheetData.csv', index=False)
 
 # Fetch Cash Flow Statement Data 
-cashFlowStatementURL = 'https://www.alphavantage.co/query?function=CASH_FLOW&symbol=' + ticker + '&apikey=H1PL45YBNI1IZDJU'
+cashFlowStatementURL = 'https://www.alphavantage.co/query?function=CASH_FLOW&symbol=' + ticker + '&apikey=' + apikey
 requestCashFlowStatement = requests.get(cashFlowStatementURL)
 cashFlowStatementData = requestCashFlowStatement.json()
 df = pd.DataFrame(cashFlowStatementData['annualReports'])
